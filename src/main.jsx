5039e8f941369636d618dd954106103bffb0d617
김상구 <spair0039@gmail.com>
2023-12-29 09:41:27 +0900

    add fonts

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import { Global } from '@emotion/react'
+import { GlobalStyle } from './style/globalStyle.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+  <Global styles={GlobalStyle}/>
     <App />
   </React.StrictMode>,
 )
---
+++ src/style/globalStyle.jsx
@@ -0,0 +1,15 @@
+import { css } from '@emotion/react'
+
+export const GlobalStyle = css`
+    * {
+        font-family: 'Wanted Sans';
+        font-feature-settings: 'cv10' on, 'cv14' on, 'cv16' on, 'cv17' on, 'cv22' on;
+        margin: 0;
+        box-sizing: border-box;
+    }
+
+    @font-face {
+        font-family: 'Wanted Sans';
+        src: url('/public/font/WantedSans.woff2') format('woff2');
+    }
+`
