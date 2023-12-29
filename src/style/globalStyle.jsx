93acaddca4d2500ff775f8089ead27e0788846bd
김상구 <spair0039@gmail.com>
2023-12-29 10:25:56 +0900

    define root style

--- src/style/globalStyle.jsx
+++ src/style/globalStyle.jsx
@@ -6,6 +6,14 @@ export const GlobalStyle = css`
         font-feature-settings: 'cv10' on, 'cv14' on, 'cv16' on, 'cv17' on, 'cv22' on;
         margin: 0;
         box-sizing: border-box;
+        color: #ffffff;
+    }
+    #root{
+        display: flex;
+        background-color: #000000;
+        height: 100dvh;
+        align-items: center;
+        justify-content: center;
     }
 
     @font-face {
