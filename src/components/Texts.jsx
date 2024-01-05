3a00da690d08208369161bb6e79bfae21db470b3
김상구 <spair0039@gmail.com>
2024-01-05 19:04:26 +0900

    ✨ Text Compoenents

---
+++ src/components/Texts.jsx
@@ -0,0 +1,39 @@
+import styled from '@emotion/styled'
+
+/**
+ * a Text Styles
+ * @param {object} props 
+ * @param {React.ReactNode} props.children
+ * @param {string} props.text 
+ * @returns {React.ReactNode}
+ */
+export const Text36Bold = styled.div`
+    font-size: 36px;
+    font-weight: 700;
+    width: 100%;
+    text-align: ${({$align})=>$align};
+`
+export const Text32Bold = styled.div`
+    font-size: 32px;
+    font-weight: 700;
+    width: 100%;
+    text-align: ${({$align})=>$align};
+`
+export const Text24Regular = styled.div`
+    font-size: 24px;
+    font-weight: 500;
+    width: 100%;
+    text-align: ${({$align})=>$align};
+`
+export const Text20Regular = styled.div`
+    font-size: 20px;
+    font-weight: 500;
+    width: 100%;
+    text-align: ${({$align})=>$align};
+`
+export const Text16Regular = styled.div`
+    font-size: 16px;
+    font-weight: 500;
+    width: 100%;
+    text-align: ${({$align})=>$align};
+`
