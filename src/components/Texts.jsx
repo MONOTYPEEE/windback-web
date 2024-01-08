aaa776d6ae2f1648afe370e2ec9bfa7e438a7334
김상구 <spair0039@gmail.com>
2024-01-08 11:31:54 +0900

    ✨ add noFill, color atribute

--- src/components/Texts.jsx
+++ src/components/Texts.jsx
@@ -4,36 +4,44 @@ import styled from '@emotion/styled'
  * a Text Styles
  * @param {object} props 
  * @param {React.ReactNode} props.children
- * @param {string} props.text 
+ * @param {string} props.text
+ * @param {boolean} props.$noFill
+ * @param {string} props.$color
+ * @param {'start'|'end'|'left'|'right'|'center'|'justify'} props.$align
  * @returns {React.ReactNode}
  */
 export const Text36Bold = styled.div`
     font-size: 36px;
     font-weight: 700;
-    width: 100%;
+    width: ${({$noFill})=> !$noFill && '100%'};
     text-align: ${({$align})=>$align};
+    color: ${({$color})=> $color ? $color : null};
 `
 export const Text32Bold = styled.div`
     font-size: 32px;
     font-weight: 700;
-    width: 100%;
+    width: ${({$noFill})=> !$noFill && '100%'};
     text-align: ${({$align})=>$align};
+    color: ${({$color})=> $color ? $color : null};
 `
 export const Text24Regular = styled.div`
     font-size: 24px;
     font-weight: 500;
-    width: 100%;
+    width: ${({$noFill})=> !$noFill && '100%'};
     text-align: ${({$align})=>$align};
+    color: ${({$color})=> $color ? $color : null};
 `
 export const Text20Regular = styled.div`
     font-size: 20px;
     font-weight: 500;
-    width: 100%;
+    width: ${({$noFill})=> !$noFill && '100%'};
     text-align: ${({$align})=>$align};
+    color: ${({$color})=> $color ? $color : null};
 `
 export const Text16Regular = styled.div`
     font-size: 16px;
     font-weight: 500;
-    width: 100%;
+    width: ${({$noFill})=> !$noFill && '100%'};
     text-align: ${({$align})=>$align};
+    color: ${({$color})=> $color ? $color : null};
 `
