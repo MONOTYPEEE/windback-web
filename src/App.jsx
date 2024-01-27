164ffe992a14c4413e880421825d1bde7c077dd6
김상구 <spair0039@gmail.com>
2024-01-27 16:33:43 +0900

    ✨ basic routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
-function App() {
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Main from "./pages/main"
 
-  return (
-    <>
-      hello
-    </>
-  )
+function App() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="*"/>
+                <Route path="/" element={<Main/>}/>
+                <Route path="/servers"/>
+                <Route path="/auth"/>
+                <Route path="/rewind/*"/>
+            </Routes>
+        </BrowserRouter>
+    )
 }
 
 export default App
