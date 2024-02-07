5f13dd36d51b9f909ac6ea99b47f8dd7b5c97d3e
김상구 <spair0039@gmail.com>
2024-02-07 16:36:38 +0900

    🎨 adding DuoIcons

--- src/pages/stories/IntroMain.jsx
+++ src/pages/stories/IntroMain.jsx
@@ -8,7 +8,7 @@ export default function IntroMain(){
     const servername = '놀이터'
     return(
         <Center>
-            <ServerIcon src="/image/demo.png"/>
+            <DuoIcons server="/image/demo.png" profile="/image/demo.png"/>
             <Text36Bold $align='center'>{servername} 연말결산</Text36Bold>
             <Text16Regular $align='center' $color='#FFFFFF88'>by WindBack</Text16Regular>
             <Text20Regular $align='center'>userName님의 올 해 돌아보기</Text20Regular>
@@ -17,9 +17,29 @@ export default function IntroMain(){
     )
 }
 
-const ServerIcon = styled.img`
-    width: 96px;
-    height: 96px;
+function DuoIcons({server, profile}){
+    return(
+        <div>
+            <ServerIcon src={server}/>
+            <UserProfile src={profile}/>
+        </div>
+    )
+}
 
-    border-radius: 99px;
+const ServerIcon = styled.img`
+    height: 96px;
+    width: 96px;
+
+    border-radius: 88px;
+
+    clip-path: url('/image/Subtract.svg#shape');
+`
+
+const UserProfile = styled.img`
+    height: 48px;
+    width: 48px;
+
+    margin-left: -24px;
+
+    border-radius: 88px;
 `
