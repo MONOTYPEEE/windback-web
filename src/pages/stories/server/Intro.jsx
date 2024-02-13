1d5a5a6e0ddda93fba402b9dcd70c4aee2168e72
김상구 <spair0039@gmail.com>
2024-02-13 16:34:03 +0900

    📄 reflect design changing

--- src/pages/stories/server/Intro.jsx
+++ src/pages/stories/server/Intro.jsx
@@ -1,9 +1,14 @@
-import Center from '../../../components/layout/Center';
+import { Center } from '../../../components/layout/Center';
+import Button from  '../../../components/Button';
+import { Text36Bold } from '../../../components/Texts';
 
 export default function ServerIntro(){
     return(
         <Center>
-            
+            <Text36Bold $align='center'>
+                Acme에서 가장 많이 선택받은 것들이에요
+            </Text36Bold>
+            <Button text='다음'/>
         </Center>
     )
 }
