e9b659ae1ecbd276b420eb3d9018b51d7370210e
김상구 <spair0039@gmail.com>
2024-03-05 15:20:02 +0900

    📄 Happy

--- src/pages/stories/award/Happy.jsx
+++ src/pages/stories/award/Happy.jsx
@@ -1,12 +1,22 @@
+import AwardProfile from "../../../components/Awardprofile";
 import { Center } from "../../../components/layout/Center";
-import { Text36Bold } from "../../../components/Texts";
+import { Text24Regular, Text36Bold } from "../../../components/Texts";
+
+const Data = {
+    name: '니간우',
+    id: '984407929048432701',
+    tag: 'kimkw',
+    count: 672,
+    icon: 'eac8e9a725a38f99dd21120881088ad9'
+}
 
 export default function AwardHappy(){
     return(
         <>
             <Text36Bold>엌ㅋㅋㅋㅋㅋ</Text36Bold>
             <Center>
-                
+                <AwardProfile {...Data}/>
+                <Text24Regular $align="center">ㅋㅋㅋ 9283회 ㅋㅋㅋㅋ</Text24Regular>
             </Center>
         </>
     )
