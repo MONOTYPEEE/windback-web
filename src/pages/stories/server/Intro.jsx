4c25e03de76c2137aaf53177e93c4c977576ab4e
김상구 <spair0039@gmail.com>
2024-03-06 20:09:24 +0900

    ✨ add Previous Button on intro pages

--- src/pages/stories/server/Intro.jsx
+++ src/pages/stories/server/Intro.jsx
@@ -3,7 +3,7 @@ import Button from  '../../../components/Button';
 import { Text36Bold } from '../../../components/Texts';
 import { PageContext } from '../../handler/StoryProvider';
 import { useContext } from 'react';
-import { getNextStory } from '../../../utils/storyUtil';
+import { getNextStory, getPreviousStory } from '../../../utils/storyUtil';
 
 export default function ServerIntro(){
     const PageState = useContext(PageContext)
@@ -14,6 +14,7 @@ export default function ServerIntro(){
                 Acme에서 가장 많이 선택받은 것들이에요
             </Text36Bold>
             <Button text='다음' onClick={()=> PageState.set(getNextStory(PageState.state))}/>
+            <Button text='이전' color='transparent' onClick={()=>PageState.set(getPreviousStory(PageState.state))}/>
         </Center>
     )
 }
