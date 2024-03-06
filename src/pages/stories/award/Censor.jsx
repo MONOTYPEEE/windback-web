76b221242284aaa564e63227e2fc11863fb1ab3a
김상구 <spair0039@gmail.com>
2024-03-06 09:25:05 +0900

    📄 Censor

--- src/pages/stories/award/Censor.jsx
+++ src/pages/stories/award/Censor.jsx
@@ -1,12 +1,22 @@
+import AwardProfile from "../../../components/Awardprofile";
 import { Center } from "../../../components/layout/Center";
-import { Text36Bold } from "../../../components/Texts";
+import { Text24Regular, Text36Bold } from "../../../components/Texts";
+
+const Data = {
+    name: '니간우',
+    id: '984407929048432701',
+    tag: 'kimkw',
+    count: 672,
+    icon: 'eac8e9a725a38f99dd21120881088ad9'
+}
 
 export default function AwardCensor(){
     return(
         <>
-            <Text36Bold>(자체적으로 검열됨)</Text36Bold>
+            <Text36Bold>(검열됨)</Text36Bold>
             <Center>
-                
+                <AwardProfile {...Data}/>
+                <Text24Regular>메시지 수정·삭제 268회</Text24Regular>
             </Center>
         </>
     )
