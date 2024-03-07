ff29cb072d80c4d0eb8c335d03546f26bb5e6ff7
김상구 <spair0039@gmail.com>
2024-03-07 11:06:22 +0900

    🎨 Texts rem

--- src/components/Texts.jsx
+++ src/components/Texts.jsx
@@ -11,49 +11,49 @@ import styled from '@emotion/styled'
  * @returns {React.ReactNode}
  */
 export const Text48Black = styled.div`
-    font-size: 48px;
+    font-size: 3rem;
     font-weight: 900;
     width: ${({$noFill})=> !$noFill && '100%'};
     text-align: ${({$align})=>$align};
     color: ${({$color})=> $color ? $color : null};
 `
 export const Text36Bold = styled.div`
-    font-size: 36px;
+    font-size: 2.25rem;
     font-weight: 700;
     width: ${({$noFill})=> !$noFill && '100%'};
     text-align: ${({$align})=>$align};
     color: ${({$color})=> $color ? $color : null};
 `
 export const Text32Bold = styled.div`
-    font-size: 32px;
+    font-size: 2rem;
     font-weight: 700;
     width: ${({$noFill})=> !$noFill && '100%'};
     text-align: ${({$align})=>$align};
     color: ${({$color})=> $color ? $color : null};
 `
 export const Text28Bold = styled.div`
-    font-size: 28px;
+    font-size: 1.75rem;
     font-weight: 700;
     width: ${({$noFill})=> !$noFill && '100%'};
     text-align: ${({$align})=>$align};
     color: ${({$color})=> $color ? $color : null};
 `
 export const Text24Regular = styled.div`
-    font-size: 24px;
+    font-size: 1.5rem;
     font-weight: 500;
     width: ${({$noFill})=> !$noFill && '100%'};
     text-align: ${({$align})=>$align};
     color: ${({$color})=> $color ? $color : null};
 `
 export const Text20Regular = styled.div`
-    font-size: 20px;
+    font-size: 1.25rem;
     font-weight: 500;
     width: ${({$noFill})=> !$noFill && '100%'};
     text-align: ${({$align})=>$align};
     color: ${({$color})=> $color ? $color : null};
 `
 export const Text16Regular = styled.div`
-    font-size: 16px;
+    font-size: 1rem;
     font-weight: 500;
     width: ${({$noFill})=> !$noFill && '100%'};
     text-align: ${({$align})=>$align};
